b05f65ba37995e8b675445c144010d7b22a62170
DaviMarques <davimarquesr@gmail.com>
2022-12-20 20:34:46 -0300

    url shorten controller criado

--- controllers/urls.controllers.js
+++ controllers/urls.controllers.js
@@ -1,5 +1,22 @@
+import { nanoid } from "nanoid";
 import { connection } from "../database/server.js";
 
 export async function encurtaUrl(req, res) {
   const { url } = req.body;
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+  try {
+    const urlEncurtada = nanoid();
+    const RecebeId = await connection.query(
+      'SELECT "usuarioId" FROM sessoes WHERE token = $1;',
+      [token]
+    );
+    await connection.query(
+      'INSERT INTO urls("usuarioId",url, "shortUrl") VALUES ($1,$2,$3);',
+      [RecebeId.rows[0].usuarioId, url, urlEncurtada]
+    );
+    res.status(201).send({ shortUrl: urlEncurtada });
+  } catch (err) {
+    res.sendStatus(400);
+  }
 }
